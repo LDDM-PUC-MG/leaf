0a66835bf14be25d8eb1f07da5a34544cbe8fc02
Vitor Militão <127860807+militaovitor01@users.noreply.github.com>
2024-10-17 11:58:23 -0300

    Update colors.dart

--- lib/styles/colors.dart
+++ lib/styles/colors.dart
@@ -1,9 +1,16 @@
 import 'dart:ui';
 
 class AppColors {
-  static const Color primary = Color(0xFF4C3CC3); // Cor primária
-  static const Color secondary = Color(0xFF4696E0); // Cor secundária
-  static const Color background = Color(0xFFFFFFFF); // Branco
+  static const Color primary = Color(0xff588157); // Cor primária
+  static const Color secondary = Color(0xff99a477); // Cor secundária
+  static const Color background = Color(0xFFF4F9E9); // Branco
   static const Color textPrimary = Color(0xFF000000); // Preto
   static const Color backgroundSecondary = Color(0xFFDDD5D5); //Cinza
 }
+
+//  static const Color primary = Color(0xff6247aa); // Cor primária
+//  static const Color secondary = Color(0xFF7251b5); // Cor secundária
+
+//  static const Color primary = Color(0xff588157); // Cor primária
+//  static const Color secondary = Color(0xff99a477); // Cor secundária
+//  static const Color secondary = Color(0xff91b48f); // Cor secundária
